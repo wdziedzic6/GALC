4b1b07baca99a226d4b11406c051c84676e487cf
irek <irek140@poczta.fm>
2022-05-28 10:16:50 +0200

    commit

--- classifiers/DecisionTreeClassifier.py
+++ classifiers/DecisionTreeClassifier.py
@@ -11,31 +11,12 @@ class DecisionTreeClassifier:
 
     def classify(self, training_data, test_data, percentage_range):
         print("Start classification with DecisionTreeClassifier")
-        tr_dataset = pd.read_csv(training_data)
-        # test_dataset = pd.read_csv(test_data)
-        print("tr_dataset=", tr_dataset)
-        # print("test_dataset=", test_dataset)
 
-        noColumn = tr_dataset.shape[1]  # Ustalenie liczby kolumn w danych
-        features = tr_dataset.iloc[:, :noColumn - 1]  # Wyodrębnienie częśći warunkowej danych
-        labels = tr_dataset.iloc[:, [noColumn - 1]]  # Wyodrębnienie kolumny decyzyjnej
-
-        datasets = train_test_split(features, labels, test_size=0.6, random_state=1234)
-        features_train = datasets[0]
-        features_test = datasets[1]
-        labels_train = datasets[2]
-        labels_test = datasets[3]
-
-        print("features_train=", features_train)
-        print("features_test=", features_test)
-        print("labels_train=", labels_train)
-        print("labels_test=", labels_test)
-
-        objects_with_headers = utils.get_objects(training_data)
-        objects_without_headers = objects_with_headers[1:]
+        # objects_with_headers = utils.get_objects(training_data)
+        # objects_without_headers = objects_with_headers[1:]
 
         test_dataset = utils.get_objects(test_data)
-        print("test_dataset=", test_dataset)
+        test_dataset_pd = pd.read_csv(test_data)
 
         my_criterion = "gini"  # Kryterium podziału węzła drzewa podczas budowy drzewa: 'gini', 'entropy'
         my_max_depth = 5
@@ -57,19 +38,40 @@ class DecisionTreeClassifier:
             # training_set_for_object = self.prepare_the_most_similar_data()
             # Tu musi zostać zaimplementowane wybranie najbardziej podobnych obiektów do testowego (nowy zbiór treningowy)
 
-            print("Analizowany obiekt #=", i)
-            print("len(test_dataset)=", len(test_dataset))
+            print("Analizowany obiekt #", i)
+            print("Atrybuty test_dataset[i]:", test_dataset[i])
 
-            training_set_for_object_without_decision = utils.get_objects_without_decision(objects_without_headers)
-            decisions_set_for_object = utils.get_decisions_set(objects_without_headers)
+            tr_dataset = pd.read_csv(training_data)
+            # tu trzeba zrobić zwrócenie zbioru treningowego najbardizej podobnych obiektów do obiektu testowego
+            # i musi być rozgraniczenie na zmienną z samymi tylko atrybutami warunkowymi i na zmienną
+            # z samymi tylko etykietami
+            # te zmienne potem posłużą w metodzie fit do zbudowania klasyfikatora
 
-            conditional_attributes = training_set_for_object_without_decision
-            decision_attributes = decisions_set_for_object
+            no_column = tr_dataset.shape[1]  # Ustalenie liczby kolumn w danych
+            features = tr_dataset.iloc[:, :no_column - 1]  # Wyodrębnienie częśći warunkowej danych
+            labels = tr_dataset.iloc[:, [no_column - 1]]  # Wyodrębnienie kolumny decyzyjnej
+
+            datasets = train_test_split(features, labels, test_size=0.6, random_state=1234)
+            features_train = datasets[0]
+            features_test = datasets[1]
+            labels_train = datasets[2]
+            labels_test = datasets[3]
+
+            # training_set_for_object_without_decision = utils.get_objects_without_decision(objects_without_headers)
+            # decisions_set_for_object = utils.get_decisions_set(objects_without_headers)
+            # conditional_attributes = training_set_for_object_without_decision
+            # decision_attributes = decisions_set_for_object
 
             # model.fit(training_set_for_object_without_decision, np.ravel(decisions_set_for_object))
             model.fit(features_train, np.ravel(labels_train))
 
-            labels_predicted = model.predict(features_test) # Generowanie decyzji dla obiektu testowego
+            print("test test_object_data_frame: ", test_object_data_frame)
+            test_object_data_frame = test_dataset_pd.iloc[[i], :]
+            print("test_object_data_frame: ", test_object_data_frame)
+            print("type(test_object_data_frame): ", type(test_object_data_frame))
+
+            labels_predicted = model.predict(test_object_data_frame) # Generowanie decyzji dla obiektu testowego
+            # labels_predicted = model.predict(features_test) # Generowanie decyzji dla obiektu testowego
             print("labels_predicted=", labels_predicted)
 
         accuracy_of_classification = 0  # Tu wyznaczyć dokładność klasyfikacji
